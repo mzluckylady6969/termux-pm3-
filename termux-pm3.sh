8c628abc122ccb505b2c1ab7becd89845522fb3b
mzluckylady6969 <117005118+mzluckylady6969@users.noreply.github.com>
2024-05-24 10:24:22 -0600

    Update termux-pm3.sh
    Signed-off-by: mzluckylady6969 <117005118+mzluckylady6969@users.noreply.github.com>

--- termux-pm3.sh
+++ termux-pm3.sh
@@ -37,7 +37,7 @@ libbz2-dev liblz4-dev libbluetooth-dev libpython3-dev libssl-dev libgd-dev libc+
     # Get the proxmark3 RDV4 repository.
     echo "[*] Get the Proxmark3 RDV4 repository"
     git clone https://github.com/RfidResearchGroup/proxmark3.git
-    got clone https://github.com/lz4/lz4
+    git clone https://github.com/lz4/lz4
     cd lz4
     make install 
     cd ..
@@ -47,7 +47,7 @@ libbz2-dev liblz4-dev libbluetooth-dev libpython3-dev libssl-dev libgd-dev libc+
 elif [ "$1" == "run" ]; then
     # Run Proxmark3 client (needs root for now).
     echo "[*] Run the Proxmark3 RDV4 client as root on /dev/ttyACM0"
-    su -c 'cd proxmark3/client && ./proxmark3 -p /dev/ttyACM0'
+    sudo -c 'cd proxmark3/client && ./proxmark3 -p /dev/ttyACM0'
 elif [ "$1" == "update" ]; then
     echo "[*] Update the Proxmark3 RDV4 repository"
     cd proxmark3
@@ -61,27 +61,3 @@ else
          "Maybe then it will be possible to do this without root."
 fi
 
- *
- * The number of bytes regenerated into dstBuffer will be provided within *dstSizePtr (necessarily <= original value).
- *
- * The number of bytes effectively used from srcBuffer will be provided within *srcSizePtr (necessarily <= original value).
- * If the number of bytes read is < number of bytes provided, then the decompression operation is not complete.
- * This typically happens when dstBuffer is not large enough to contain all decoded data.
- * LZ4F_decompress() will have to be called again, starting from where it stopped (srcBuffer + *srcSizePtr)
- * The function will check this condition, and refuse to continue if it is not respected.
- * dstBuffer is supposed to be flushed between calls to the function, since its content will be rewritten.
- * Different dst arguments can be used between each calls.
- *
- * The function result is an hint of the better srcSize to use for next call to LZ4F_decompress.
- * Basically, it's the size of the current (or remaining) compressed block + header of next block.
- * Respecting the hint provides some boost to performance, since it allows less buffer shuffling.
- * Note that this is just a hint, you can always provide any srcSize you want.
- * When a frame is fully decoded, the function result will be 0.
- * If decompression failed, function result is an error code which can be tested using LZ4F_isError().
- */
-
-
-
-#if defined (__cplusplus)
-}
-#endif
